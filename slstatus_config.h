40a3b79c027f5ae465d96d4c780bb388cdd48204
Bryan Bennett <Bryan.Bennett@proton.me>
2024-11-25 20:23:00 -0500

    Add battery percentage to slstatus

--- slstatus_config.h
+++ slstatus_config.h
@@ -6,4 +6,5 @@ static const struct arg args[] = {
   { cpu_perc, "^fg(D3C6AA)%s%% ", NULL},
   { ram_perc, "^fg(a7C080)%s%% ", NULL},
   { battery_state, "^fg(D3C6AA)%s ", "BAT0"},
+  { battery_perc, "^fg(D3C6AA)%s%% ", "BAT0"},
 };
