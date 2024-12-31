5f943b787e68bb232695ace1ea0f7b933b8c1ac9
Bryan Bennett <Bryan.Bennett@proton.me>
2024-12-31 12:56:20 -0500

    Inject basic autostart script

--- flake.nix
+++ flake.nix
@@ -3,45 +3,61 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let pkgs = import nixpkgs { system ="x86_64-linux"; };
-        getExe = pkgs.lib.meta.getExe;
-        dwl = pkgs.dwl.overrideAttrs(oldAttrs: {
-          src = ./.;
-          buildInputs = oldAttrs.buildInputs ++ (builtins.attrValues {
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      getExe = pkgs.lib.meta.getExe;
+      autostart = pkgs.writeScriptBin "dwl_autostart.sh" ''
+        ${getExe pkgs.wbg} ~/pictures/wallpaper.jpg &
+        kanshi &
+        emacs &
+        librewolf &
+      '';
+
+      dwl = pkgs.dwl.overrideAttrs (oldAttrs: {
+        src = ./.;
+        buildInputs =
+          oldAttrs.buildInputs
+          ++ (builtins.attrValues {
             inherit (pkgs) libdrm fcft libinput;
           });
-          patchPhase = ''
-            substituteInPlace \
-              config.def.h \
-              --replace-fail "kitty" ${getExe pkgs.kitty} \
-              --replace-fail "bemenu-run" "${pkgs.bemenu}/bin/bemenu-run" \
-              --replace-fail "brightnessctl" ${getExe pkgs.brightnessctl} \
-              --replace-fail "pamixer" ${getExe pkgs.pamixer} \
-              --replace-fail "swaylock" ${getExe pkgs.swaylock}
+        patchPhase = ''
+          substituteInPlace \
+            config.def.h \
+            --replace-fail "kitty" ${getExe pkgs.kitty} \
+            --replace-fail "bemenu-run" "${pkgs.bemenu}/bin/bemenu-run" \
+            --replace-fail "brightnessctl" ${getExe pkgs.brightnessctl} \
+            --replace-fail "pamixer" ${getExe pkgs.pamixer} \
+            --replace-fail "swaylock" ${getExe pkgs.swaylock} \
+            --replace-fail "autostart.sh" ${getExe autostart}
 
-            substituteInPlace \
-              dwl.c \
-              --replace-fail "/bin/sh" "${getExe pkgs.bash}"
-          '';
-        });
-        slstatus = pkgs.slstatus.overrideAttrs(oldAttrs: {
-          preBuild = ''
-            cp ${./slstatus_config.h} config.h
-          '';
-        });
-        dwls = pkgs.writeScriptBin "dwls" ''
-          ${getExe slstatus} -s | ${getExe dwl}
+          substituteInPlace \
+            dwl.c \
+            --replace-fail "/bin/sh" "${getExe pkgs.bash}"
         '';
-     in {
+      });
+      slstatus = pkgs.slstatus.overrideAttrs (oldAttrs: {
+        preBuild = ''
+          cp ${./slstatus_config.h} config.h
+        '';
+      });
+      dwls = pkgs.writeScriptBin "dwls" ''
+        ${getExe slstatus} -s | ${getExe dwl}
+      '';
+    in
+    {
       packages.x86_64-linux = rec {
         inherit dwl dwls;
-        
+
         default = dwls;
       };
       devShells.x86_64-linux.default = pkgs.mkShell {
         packagesFrom = dwl;
-        packages = [ pkgs.pkg-config pkgs.ccls ];
+        packages = [
+          pkgs.pkg-config
+          pkgs.ccls
+        ];
       };
-  };
+    };
 }
